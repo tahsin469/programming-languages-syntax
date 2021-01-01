0782f9f65116a8ea96b489a5f0be0961a95f33e4
Md.Tahsinul Islam <mughdaw825@gmail.com>
2021-01-01 23:52:15 +0600

    Add files via upload

---
+++ c++ syntax/Function Overriding/C++ Function Overriding.cpp
@@ -0,0 +1,26 @@
+// C++ program to demonstrate function overriding
+
+#include <iostream>
+using namespace std;
+
+class Base {
+   public:
+    void print() {
+        cout << "Base Function" << endl;
+    }
+};
+
+class Derived : public Base {
+   public:
+    void print() {
+        cout << "Derived Function" << endl;
+    }
+};
+
+int main() {
+    Derived derived1;
+    //derived1.Base::print();
+    derived1.print();
+
+    return 0;
+}
