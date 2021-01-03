b55db1bbed943de9eeed009707db3e0f038278f3
Md.Tahsinul Islam <mughdaw825@gmail.com>
2021-01-03 23:00:24 +0600

    Add files via upload

---
+++ c++ syntax/References vs Pointers/Creating References in C++.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+   // declare simple variables
+   int    i;
+   double d;
+
+   // declare reference variables
+   int&    r = i;
+   double& s = d;
+
+   i = 5;
+   cout << "Value of i : " << i << endl;
+   cout << "Value of i reference : " << r  << endl;
+
+   d = 11.7;
+   cout << "Value of d : " << d << endl;
+   cout << "Value of d reference : " << s  << endl;
+
+   return 0;
+}
+
+
+/*
+References vs Pointers
+References are often confused with pointers but three major differences between references and pointers are −
+
+You cannot have NULL references. You must always be able to assume that a reference is connected to a legitimate piece of storage.
+
+Once a reference is initialized to an object, it cannot be changed to refer to another object. Pointers can be pointed to another object at any time.
+
+A reference must be initialized when it is created. Pointers can be initialized at any time.
+*/
